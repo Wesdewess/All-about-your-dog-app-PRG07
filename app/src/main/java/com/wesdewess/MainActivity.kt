d61b741444921ab94740bbd8677fce87649403ef
Wesdewess <35997013+Wesdewess@github.com>
2020-03-09 11:55:29 +0100

    added maps activity

--- app/src/main/java/com/wesdewess/MainActivity.kt
+++ app/src/main/java/com/wesdewess/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.wesdewess
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun openMap(v: View?) {
+        val toast = Toast.makeText(applicationContext, "Yo man", Toast.LENGTH_SHORT)
+        toast.show()
+        startActivity(Intent(this@MainActivity, MapsActivity::class.java))
+        Log.d("user_action","maps button clicked")
     }
 }
