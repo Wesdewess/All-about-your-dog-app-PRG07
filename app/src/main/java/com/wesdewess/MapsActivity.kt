3eb853f4c5c1c3fd4fa01a550d0ff68c198f7609
Wesdewess <35997013+Wesdewess@users.noreply.github.com>
2020-04-08 22:53:41 +0200

    changed pin location

--- app/src/main/java/com/wesdewess/MapsActivity.kt
+++ app/src/main/java/com/wesdewess/MapsActivity.kt
@@ -26,9 +26,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        // Add a marker at Daya's home and move the camera
+        val dayaLocation = LatLng(51.984511, 4.320758)
+        mMap.addMarker(MarkerOptions().position(dayaLocation).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(dayaLocation))
     }
 }
