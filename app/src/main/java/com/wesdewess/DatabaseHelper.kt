6b8a1697410844a90025c3ae79e8104e940984a3
Wesdewess <35997013+Wesdewess@users.noreply.github.com>
2020-04-12 15:35:47 +0200

    correctly show list with dates and update it when new date is added

--- app/src/main/java/com/wesdewess/DatabaseHelper.kt
+++ app/src/main/java/com/wesdewess/DatabaseHelper.kt
@@ -7,10 +7,10 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 
-class DatabaseHelper(context: Context?) :
-    SQLiteOpenHelper(context, DATABASE_NAME, null, 1) {
+class DatabaseHelper(context: Context?) :SQLiteOpenHelper(context, DATABASE_NAME, null, 1) {
+
     override fun onCreate(db: SQLiteDatabase){
-        db.execSQL("create table $TABLE_NAME (ID INTEGER PRIMARY KEY AUTOINCREMENT,NAME TEXT,SURNAME TEXT)")
+        db.execSQL("create table $TABLE_NAME (ID INTEGER PRIMARY KEY AUTOINCREMENT,DATE TEXT)")
     }
 
     override fun onUpgrade(db: SQLiteDatabase,oldVersion: Int,newVersion: Int){
@@ -18,11 +18,10 @@ class DatabaseHelper(context: Context?) :
         onCreate(db)
     }
 
-    fun insertData(name: String?,surname: String?): Boolean{
+    fun insertData(date: String?): Boolean{
         val db = this.writableDatabase
         val contentValues = ContentValues()
-        contentValues.put(COL_2, name)
-        contentValues.put(COL_3, surname)
+        contentValues.put(COL_2, date)
         val result = db.insert(TABLE_NAME, null, contentValues)
         return result != -1L
     }
@@ -32,13 +31,24 @@ class DatabaseHelper(context: Context?) :
             val db = this.writableDatabase
             return db.rawQuery("select * from $TABLE_NAME", null)
         }
+    fun getDates(): ArrayList<HashMap<String, String>>? {
+        val db = this.writableDatabase
+        val dateList: ArrayList<HashMap<String, String>> = ArrayList()
+        val cursor = db.rawQuery("select * from $TABLE_NAME", null)
+        while (cursor.moveToNext()) {
+            val date: HashMap<String, String> = HashMap()
+            date["ID"] = cursor.getString(cursor.getColumnIndex(COL_1))
+            date["DATE"] = cursor.getString(cursor.getColumnIndex(COL_2))
+            dateList.add(date)
+        }
 
-    fun updateData(id: String,name: String?,surname: String?): Boolean{
+        return dateList
+    }
+    fun updateData(id: String,date: String?): Boolean{
         val db = this.writableDatabase
         val contentValues = ContentValues()
         contentValues.put(COL_1, id)
-        contentValues.put(COL_2, name)
-        contentValues.put(COL_3, surname)
+        contentValues.put(COL_2, date)
         db.update(TABLE_NAME, contentValues,"ID = ?", arrayOf(id))
         return true
     }
@@ -49,10 +59,9 @@ class DatabaseHelper(context: Context?) :
     }
 
     companion object {
-        const val DATABASE_NAME = "Student.db"
-        const val TABLE_NAME = "student_table"
+        const val DATABASE_NAME = "Daya.db"
+        const val TABLE_NAME = "ontworming_dates"
         const val COL_1 = "ID"
-        const val COL_2 = "NAME"
-        const val COL_3 = "SURNAME"
+        const val COL_2 = "DATE"
     }
 }
--- app/src/main/java/com/wesdewess/Ontworming.kt
+++ app/src/main/java/com/wesdewess/Ontworming.kt
@@ -1,17 +1,28 @@
 package com.wesdewess
 
-import android.app.AlertDialog
-import android.database.Cursor
+
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.widget.EditText
+import android.widget.ArrayAdapter
+import android.widget.DatePicker
+import android.widget.ListView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_ontworming.*
+import java.time.format.DateTimeFormatter
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class Ontworming : AppCompatActivity() {
-    var dayaDatabase: DatabaseHelper? = null //define which databaseHelper to use
+
+    private var dayaDatabase: DatabaseHelper? = null //define which databaseHelper to use
+    private val list = ArrayList<String?>()
+    private lateinit var adapter: ArrayAdapter<String?>
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ontworming)
@@ -19,48 +30,23 @@ class Ontworming : AppCompatActivity() {
         fab.setOnClickListener {
             showForm()
         }
+        dayaDatabase = DatabaseHelper(this)
 
-        dayaDatabase = DatabaseHelper(this);
+        val listView = findViewById<ListView>(R.id.ListView)
 
-        val res: Cursor = dayaDatabase!!.allData
-        if (res.count == 0) {
-            // show message
-            showList("Error", "Nothing found")
-            return
+        val values = dayaDatabase!!.getDates()
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
+
+        if (values != null) {
+            for (element in values) {
+                list.add(0,element["DATE"])
+            }
         }
 
-        val buffer = StringBuffer()
-        while (res.moveToNext()) {
-            buffer.append(
-                """
-                    Id :${res.getString(0).toString()}
-                    
-                    """.trimIndent()
-            )
-            buffer.append(
-                """
-                    Name :${res.getString(1).toString()}
-                    
-                    """.trimIndent()
-            )
-            buffer.append(
-                """
-                    Surname :${res.getString(2).toString()}
-                    
-                    """.trimIndent()
-            )
-        }
+        listView.adapter = adapter
+    }
 
-        // Show all data
-        showList("Items in database",buffer.toString())
-    }
-    private fun showList(title:String,data:String){
-        val builder = AlertDialog.Builder(this)
-        builder.setCancelable(true)
-        builder.setTitle(title)
-        builder.setMessage(data)
-        builder.show()
-    }
     private fun showForm() {
         val builder: AlertDialog.Builder = AlertDialog.Builder(this)
         val inflater: LayoutInflater = layoutInflater
@@ -71,8 +57,11 @@ class Ontworming : AppCompatActivity() {
         builder.apply {
             setPositiveButton(R.string.add
             ) { dialog, id ->
-                addEntry(dialogView.findViewById<EditText>(R.id.username).text.toString(), dialogView.findViewById<EditText>(R.id.password).text.toString())
+                val input = dialogView.findViewById<DatePicker>(R.id.dateInput)
 
+                addEntry(input.dayOfMonth.toString()+"-"+(input.month+1).toString()+"-"+input.year)
+                list.add(0,input.dayOfMonth.toString()+"-"+(input.month+1).toString()+"-"+input.year)
+                adapter.notifyDataSetChanged()
             }
             setNegativeButton(R.string.cancel
             ) { dialog, id ->
@@ -82,8 +71,12 @@ class Ontworming : AppCompatActivity() {
         builder.create().show()
     }
 
-    private fun addEntry(value1:String, value2:String) {
-            val isInserted: Boolean = dayaDatabase!!.insertData(value1,value2)
+    //insert user given value into database
+    private fun addEntry(value1:String) {
+            //insert data
+            val isInserted: Boolean = dayaDatabase!!.insertData(value1)
+
+            //show a toast
             if (isInserted) Toast.makeText(
                 this@Ontworming,
                 "Data Inserted",
