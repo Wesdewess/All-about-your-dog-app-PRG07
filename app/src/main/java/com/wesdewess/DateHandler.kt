73000bda06a81d58f637a66c3b05fc8973ff476d
Wesdewess <35997013+Wesdewess@users.noreply.github.com>
2020-04-12 23:26:17 +0200

    completed ontworming

--- app/src/main/java/com/wesdewess/DateHandler.kt
+++ app/src/main/java/com/wesdewess/DateHandler.kt
@@ -2,23 +2,50 @@ package com.wesdewess
 
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
 import java.util.*
+import kotlin.collections.ArrayList
 
 class DateHandler {
 
     private val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("d-M-yyyy", Locale.ENGLISH)
 
-    fun compareToCurrentDate(){
-
+    fun formatDateFromString(date: String?): LocalDate{
+        return LocalDate.parse(date, formatter)
     }
 
-    fun findMostRecentDate(dates: ArrayList<HashMap<String, String>>?){
-        val formattedDates: Array<LocalDate>? = null
-        if (dates != null) {
-            for ((key,element) in dates.withIndex()) {
+    fun compareToCurrentDate(date: String?): Long{
+        val fDate = formatDateFromString(date)
+        val currentDate = LocalDate.now()
+        return fDate.until(currentDate, ChronoUnit.DAYS)
+    }
+
+    fun compareToCurrentDate(dates: ArrayList<HashMap<String, String>>?): Long? {
+        val currentDate = LocalDate.now()
+        println("Current date: $currentDate")
+        return findMostRecentDate(dates)?.until(currentDate, ChronoUnit.DAYS)
+    }
+
+    private fun findMostRecentDate(dates: ArrayList<HashMap<String, String>>?): LocalDate? {
+        val formattedDates = ArrayList<LocalDate>()
+
+        if (dates!!.isNotEmpty()) {
+            for (element in dates) {
                 val date = LocalDate.parse(element["DATE"], formatter)
-                formattedDates?.set(key, date)
+                formattedDates.add(date)
             }
+        }else{
+            println("No values in dates array")
+            return LocalDate.now()
         }
+
+        var recentDate = formattedDates[0]
+        for (fDate in formattedDates)
+
+            if (recentDate < fDate) {
+            recentDate = fDate
+        }
+        println("Most recent date: $recentDate")
+        return recentDate
     }
 }
--- app/src/main/java/com/wesdewess/Ontworming.kt
+++ app/src/main/java/com/wesdewess/Ontworming.kt
@@ -3,26 +3,27 @@ package com.wesdewess
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.widget.ArrayAdapter
-import android.widget.DatePicker
-import android.widget.ListView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_ontworming.*
+import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
 import java.util.*
 import kotlin.collections.ArrayList
 
 
 class Ontworming : AppCompatActivity() {
 
+    private var dateHandler: DateHandler = DateHandler()
     private var dayaDatabase: DatabaseHelper? = null //define which databaseHelper to use
+
+
     private val list = ArrayList<String?>()
     private lateinit var adapter: ArrayAdapter<String?>
 
-
-
+    //all that happens when creating the Activity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ontworming)
@@ -32,19 +33,7 @@ class Ontworming : AppCompatActivity() {
         }
         dayaDatabase = DatabaseHelper(this)
 
-        val listView = findViewById<ListView>(R.id.ListView)
-
-        val values = dayaDatabase!!.getDates()
-
-        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
-
-        if (values != null) {
-            for (element in values) {
-                list.add(0,element["DATE"])
-            }
-        }
-
-        listView.adapter = adapter
+        makeList()
     }
 
     private fun showForm() {
@@ -58,9 +47,11 @@ class Ontworming : AppCompatActivity() {
             setPositiveButton(R.string.add
             ) { dialog, id ->
                 val input = dialogView.findViewById<DatePicker>(R.id.dateInput)
+                val addedDate = input.dayOfMonth.toString()+"-"+(input.month+1).toString()+"-"+input.year
 
-                addEntry(input.dayOfMonth.toString()+"-"+(input.month+1).toString()+"-"+input.year)
-                list.add(0,input.dayOfMonth.toString()+"-"+(input.month+1).toString()+"-"+input.year)
+                addEntry(addedDate)
+                list.add(1,addedDate + "  -  " + dateHandler.compareToCurrentDate(addedDate).toString() + " " + resources.getString(R.string.days_ago))
+
                 adapter.notifyDataSetChanged()
             }
             setNegativeButton(R.string.cancel
@@ -76,7 +67,7 @@ class Ontworming : AppCompatActivity() {
             //insert data
             val isInserted: Boolean = dayaDatabase!!.insertData(value1)
 
-            //show a toast
+            //show a toast to indicate success or failure
             if (isInserted) Toast.makeText(
                 this@Ontworming,
                 "Data Inserted",
@@ -86,5 +77,28 @@ class Ontworming : AppCompatActivity() {
                 "Failed to add data",
                 Toast.LENGTH_LONG
             ).show()
+    }
+
+    private fun makeList(){
+
+        val listView = findViewById<ListView>(R.id.ListView)
+        val values = dayaDatabase!!.getDates()
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
+
+        list.add(0, resources.getString(R.string.days_since_last_ontw) + " " + dateHandler.compareToCurrentDate(values))
+
+        if (values != null) {
+            for (element in values) {
+                list.add(1,element["DATE"] + "  -  " + dateHandler.compareToCurrentDate(element["DATE"]).toString() + " " + resources.getString(R.string.days_ago))
+            }
+        }
+
+        listView.adapter = adapter
+
+        Toast.makeText(
+            this@Ontworming,
+            "Days since last ontworming: " + dateHandler.compareToCurrentDate(values),
+            Toast.LENGTH_LONG
+        ).show()
     }
 }
