a3e0cb33791d2105d5f9a0ab1b720ef1078ed476
Wesdewess <35997013+Wesdewess@users.noreply.github.com>
2020-04-14 22:02:48 +0200

    werkende settings en fetch dog api

--- app/src/main/java/com/wesdewess/MainActivity.kt
+++ app/src/main/java/com/wesdewess/MainActivity.kt
@@ -1,19 +1,58 @@
 package com.wesdewess
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceManager
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONException
+import org.json.JSONObject
 
 
 class MainActivity : AppCompatActivity() {
-
+    val LOG_TAG = "wessel"
+    private val DOG_URI = "https://dog.ceo/api/breeds/image/random"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
+        val queue = Volley.newRequestQueue(this)
+        
+        // make request
+        // Request a string response from the provided URL.
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET,
+            DOG_URI,
+            null,
+            Response.Listener { response -> // TODO: Handle response
+                Log.d(LOG_TAG, "Success fetching JSON")
+                processData(response!!)
+            },
+            Response.ErrorListener { // TODO: Handle error
+                Log.d(LOG_TAG, "Error fetching JSON")
+            })
+        queue.add(jsonObjectRequest);
     }
+
+    @SuppressLint("SetTextI18n")
+    override fun onResume() {
+        super.onResume()
+        val dogname = PreferenceManager.getDefaultSharedPreferences(this).getString("dogname", "")
+        if (!dogname.equals("")) {
+            findViewById<TextView>(R.id.textView).text = resources.getString(R.string.titlepart) + " " + dogname
+        } else {
+            findViewById<TextView>(R.id.textView).text = resources.getString(R.string.title)
+        }
+    }
+
     //open the map activity
     fun openMap(v: View?) {
         startActivity(Intent(this@MainActivity, MapsActivity::class.java))
@@ -21,5 +60,21 @@ class MainActivity : AppCompatActivity() {
     //open the ontworming activity
     fun openOntworming(v: View?) {
         startActivity(Intent(this@MainActivity, Ontworming::class.java))
+    }
+    //open the Settings activity
+    fun openSettings(v: View?) {
+        startActivity(Intent(this@MainActivity, SettingsActivity::class.java))
+    }
+
+    fun processData(data: JSONObject) {
+        try {
+            val resultURL = data["message"] as String
+
+            Log.d(LOG_TAG, resultURL)
+
+        } catch (e: JSONException) {
+            e.printStackTrace()
+            Log.d(LOG_TAG, "Error processing JSON")
+        }
     }
 }
